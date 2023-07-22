eb52d18c0662e0ba8850961d1ad1edd05ef0be5e
mcjahn <131519880+mcjahn@users.noreply.github.com>
2023-07-22 15:18:45 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,12 +6,13 @@ setenforce 0
 systemctl stop firewalld
 systemctl disable firewalld
 #установка репозиториев, пакетов
-yum install epel-release httpd git mc -y
-yum install nginx -y
+yum install -y epel-release httpd git mc
+yum install -y nginx
 git clone https://github.com/mcjahn/otus1.git
 cd /home/mamaev/otus1
 cp default.conf /etc/nginx/conf.d/
 cp httpd.conf /etc/httpd/conf.d/default.conf
+cp httpd1.conf /etc/httpd/conf/httpd.conf
 cd /var/www/html/
 mkdir /var/www/html1
 mkdir /var/www/html2
@@ -22,3 +23,4 @@ cp /home/mamaev/otus1/index2.html /var/www/html2/index.html
 systemctl enable --now nginx
 systemctl enable --now httpd
 echo "всё сделано!"
+
